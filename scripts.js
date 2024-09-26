c1d80b4e9dec47714e448c13690fa1c48b4a48a2
Sergio Ortega <ortegagarrido.sergio@gmail.com>
2024-09-26 13:09:52 +0200

    Ejercicio terminado

--- scripts.js
+++ scripts.js
@@ -10,7 +10,7 @@ document.querySelector(".infocard-list.infocard-list-pkmn-lg").style.backgroundC
 
 // 3. Imprime por consola la URL de la página.
 
-console.log(location.href);
+console.log(window.location.href);
 
 // 4. Imprime por consola el dominio de la página.
 
@@ -18,14 +18,33 @@ console.log(window.location.hostname);
 
 // 5. Imprime todos los nodos de imagen.
 
-
+console.log(document.querySelectorAll("img"));
 
 // 6. Sustituye el atributo "src" de todas las imágenes por este "https://media.giphy.com/media/2v170e71aanfi/giphy.gif"
 
 // Seleccionamos todas las imágenes dentro de los enlaces dentro de las infocard
-let imagenes = document.querySelectorAll(".infocard-lg-img > a > img");
-    for (let i = 0; i <= imagenes.length; i++) {
-        imagenes[i].src = "https://media.giphy.com/media/2v170e71aanfi/giphy.gif";
-    }
+let imagenes = document.querySelectorAll("img");
+
+imagenes.forEach(imagen => {    // Bucle que pasa por cada una de las imagenes 
+    imagen.src = "https://media.giphy.com/media/2v170e71aanfi/giphy.gif";  // imagen.src cambia el source de todas las imágenes
+})
+/*for (let i = 0; i <= imagenes.length; i++) { // Otra forma de hacerlo con un bucle
+    imagenes[i].src = "https://media.giphy.com/media/2v170e71aanfi/giphy.gif";
+}*/
 
 // 7. Cambia el fondo de todos los infocard-lg-data text-muted para todos los Pokimon voladores itype flying
+
+    let padre;
+    // Seleccionamos todos los nodos de pokemon con class='itype flying'
+    
+    let selectorVoladores = document.querySelectorAll("main > div span.infocard-lg-data.text-muted small a[class='itype flying']");
+
+    for (let i = 0; i < selectorVoladores.length; i++) {
+        padre = selectorVoladores[i].parentNode.parentNode;
+        padre.setAttribute('style', 'background-color:black;')
+    }
+    
+
+
+
+
